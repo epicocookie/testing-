35cf26ee27450db122f08fb08f8262b91943f3d1
CookieZZ16 <lelam.vn99@gmail.com>
2026-01-28 17:14:56 +0700

    Using hashing to convert int to roman quite the same as hashmap concept but not literrally leetcode problem 12

--- int_roman.cpp
+++ int_roman.cpp
@@ -4,23 +4,31 @@ using namespace std;
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<pair<int, string>> mp = {
+        vector<pair<int, string>> roman = {
             {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
-            {100, "C"},  {90, "XC"},  {50, "L"},  {40, "XL"},
-            {10, "X"},   {9, "IX"},   {5, "V"},   {4, "IV"},
-            {1, "I"}
+            {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+            {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
         };
 
-        string res = "";
-
-        for (auto &[val, sym] : mp) {
-            if (num == 0) break;
-            int count = num / val;          // how many times val fits
-            res += string(count, '?');      // placeholder, we’ll fix next line
-            // ↑ ignore this line, not correct for multi-char symbols
-            // correct way below:
+        string result = "";
+        for (auto &a : roman) {
+            int val = a.first;
+            string letter = a.second;
+            int count = num / val; 
+            while (count--) result+=a.second; 
+            num = num%val;
         }
 
-        return res;
+        
+
+        return result;
     }
 };
+
+int main() {
+    Solution sol;
+    int num;
+    cin >> num;
+    cout << sol.intToRoman(num) << endl;
+    return 0;
+}
